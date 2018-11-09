878d9a03ed8e0e1bc3d5a342dd267d72f67ee345
剱物蒼太 <sotakenmotsu@gmail.com>
2018-11-09 19:11:26 +0900

    募集内容詳細画面遷移機能完成

---
+++ TenissBB2/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  TenissBB2
+//
+//  Created by 剱物蒼太 on 2018/11/02.
+//  Copyright © 2018年 剱物蒼太. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
